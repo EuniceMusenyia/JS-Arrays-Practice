c502c2054ff65b0d6e17858f4e34d2d539b0074f
student <eunicemusenyia@gmail.com>
2023-04-11 05:57:57 +0300

    added a binary function

--- merge.js
+++ merge.js
@@ -4,14 +4,14 @@ function divideArr(numbers) {
         
     }
     let middle = Math.floor(numbers.length/2);
-    let left = (0, middle);
-    let right = (middle);
-return sortedArr(divideArr(left), divideArr(right));
+    let left = numbers.slice(0, middle);
+    let right = numbers.slice(middle);
+return sortedArr(divideArr(left), divideArr(right))
     
 }
 
 function sortedArr(left, right) {
-    emtyArr = [0]
+    emtyArr = [];
     while (left.length && right.length){
         if (left [0]<right[0]){
             emtyArr.push(left.shift())
@@ -20,8 +20,32 @@ function sortedArr(left, right) {
             emtyArr.push(right.shift())
         }
     }
-    return[...emptyArr,...left,...right]
+    return[...emtyArr,...left,...right]
 }
 
 let numbers= [7, 3, 8, 4,5, 10,1]
 console.log(divideArr(numbers));
+
+function binary(nums, target) {
+    left = 0;
+    right = nums.length-1;
+    while (left<=right) {
+        let middle = Math.floor((left+ right)/2);
+        if (nums[middle]===target) {
+            return middle;
+            
+        }
+        else if (nums[middle]<target) {
+            left= middle+1
+            
+        }
+        else
+        right= middle-1
+
+        
+    }
+    
+}
+let nums= [2, 3, 4, 5, 7,8, 9]
+target=4
+console.log(binary(nums, target));
---
+++ practice.js
@@ -0,0 +1,21 @@
+// square root of an array
+const numbers = [3, 7, 4, 5, 9];
+const newNums = numbers.map(Math.sqrt);
+console.log(newNums);
+
+
+
+const names = ["Eunice","Musenyia","Mutunga"];
+console.log(names.toString());
+
+console.log(names.join(" "));
+
+console.log(names.unshift("Senyia"));
+
+console.log(names.shift());
+
+console.log(names.pop());
+
+console.log(names.push("Nice"));
+
+
